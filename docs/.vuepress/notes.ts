667434fbdf8567dd738499244df964b46ba5acc8
majinsuper <1533363937@qq.com>
2025-07-24 18:59:25 +0800

    1

--- docs/.vuepress/notes.ts
+++ docs/.vuepress/notes.ts
@@ -21,27 +21,27 @@
  */
 import { defineNoteConfig, defineNotesConfig } from 'vuepress-theme-plume'
 
-const LLMs_Note = defineNoteConfig({
-  dir: 'LLMs',
-  // `dir` 所指向的目录中的所有 markdown 文件，其 permalink 需要以 `link` 配置作为前缀
-  // 如果 前缀不一致，则无法生成侧边栏。
-  // 所以请确保  markdown 文件的 permalink 都以 `link` 开头
-  link: '/LLMs/',
-  // 手动配置侧边栏结构
-  // sidebar: ['', 'foo', 'bar'],
-  // 根据文件结构自动生成侧边栏
-  sidebar: 'auto',
-})
-
-// const Transformer_Note = defineNoteConfig({
-//   dir: 'transformer',
+// const LLMs_Note = defineNoteConfig({
+//   dir: 'LLMs',
 //   // `dir` 所指向的目录中的所有 markdown 文件，其 permalink 需要以 `link` 配置作为前缀
 //   // 如果 前缀不一致，则无法生成侧边栏。
 //   // 所以请确保  markdown 文件的 permalink 都以 `link` 开头
-//   link: '/transformer/',
+//   link: '/LLMs/',
+//   // 手动配置侧边栏结构
+//   // sidebar: ['', 'foo', 'bar'],
 //   // 根据文件结构自动生成侧边栏
 //   sidebar: 'auto',
 // })
+
+const Transformer_Note = defineNoteConfig({
+  dir: 'Transformer',
+  // `dir` 所指向的目录中的所有 markdown 文件，其 permalink 需要以 `link` 配置作为前缀
+  // 如果 前缀不一致，则无法生成侧边栏。
+  // 所以请确保  markdown 文件的 permalink 都以 `link` 开头
+  link: '/transformer/',
+  // 根据文件结构自动生成侧边栏
+  sidebar: 'auto',
+})
 
 /**
  * 导出所有的 note
@@ -49,7 +49,7 @@ const LLMs_Note = defineNoteConfig({
  * （DemoNote 为参考示例，如果不需要它，请删除）
  */
 export default defineNotesConfig({
-  dir: '/notes/',
+  dir: 'notes',
   link: '/',
-  notes: [LLMs_Note],
+  notes: [Transformer_Note],
 })
